f56e0f5ba6028cee592a5dc420f6efe7bacc2513
Majid Ali <67096621+Majidalee1@users.noreply.github.com>
2021-06-05 16:42:04 +0500

    Updates

---
+++ precache-manifest.2db208c719c4c776d11e4a665b861db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcf23579beacbfb6044dccfa128a7bb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ce46094c99cb11c8e9e",
+    "url": "/static/css/main.02655c6f.chunk.css"
+  },
+  {
+    "revision": "414f65a209fc020b0341",
+    "url": "/static/js/2.5cb675d9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.5cb675d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce46094c99cb11c8e9e",
+    "url": "/static/js/main.1936cada.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  }
+]);
