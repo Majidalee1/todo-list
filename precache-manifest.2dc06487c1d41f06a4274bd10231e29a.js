14007dbf1cd9c0f5f836915083b01e4002e6e0f9
Majid Ali <67096621+Majidalee1@users.noreply.github.com>
2021-06-05 16:34:10 +0500

    Updates

---
+++ precache-manifest.2dc06487c1d41f06a4274bd10231e29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9ed07f03c484708cddc68c45716e3c0",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "9ce46094c99cb11c8e9e",
+    "url": "/todo-list/static/css/main.02655c6f.chunk.css"
+  },
+  {
+    "revision": "414f65a209fc020b0341",
+    "url": "/todo-list/static/js/2.5cb675d9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/todo-list/static/js/2.5cb675d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce46094c99cb11c8e9e",
+    "url": "/todo-list/static/js/main.1936cada.chunk.js"
+  },
+  {
+    "revision": "beb2efd1e9b564ab95d9",
+    "url": "/todo-list/static/js/runtime-main.fff89ea1.js"
+  }
+]);
